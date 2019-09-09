a716eb6437acb4ab22646ed780d106260d68dde5
Daniel Ishitani <daniel.ishitani@base2.com.br>
2019-09-09 08:40:15 -0300

    section 2 components and data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   collapsed = true;
-  title = 'ng8-course-project';
+
+  loadedFeature = 'recipe';
+  
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
